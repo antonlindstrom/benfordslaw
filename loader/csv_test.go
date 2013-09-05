cdf0f46d951661f963f0a73bd33fc51beadef777
Anton Lindström <lindztr@gmail.com>
2013-09-05 17:45:25 +0200

    add test for loader csv

---
+++ loader/csv_test.go
@@ -0,0 +1,21 @@
+package loader
+
+import (
+	"testing"
+)
+
+func TestLoadCSV(t *testing.T) {
+	csv := LoadCSV("../data/FlowOfFunds.csv")
+
+	if (csv[0] != 19) {
+		t.Error("expected 19 from LoadCSV[0]")
+	}
+
+	if (csv[1] != 20) {
+		t.Error("expected 20 from LoadCSV[1]")
+	}
+
+	if (csv[54] != 12551) {
+		t.Error("expected 12551 from LoadCSV[54]")
+	}
+}
