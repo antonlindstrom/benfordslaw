c319900915f81254b96f7a8743f3e7c9ad32e27e
Anton Lindström <lindztr@gmail.com>
2013-11-09 10:19:15 +0100

    add benchmark of Populate

--- benford_test.go
+++ benford_test.go
@@ -13,3 +13,17 @@ func TestPopulate(t *testing.T) {
 		t.Fatal("Count of 0 should be 0\n")
 	}
 }
+
+func BenchmarkPopulate(b *testing.B) {
+	total := 29
+	count := []int{0,9,5,4,2,2,2,1,2,1}
+
+	for i := 0; i < b.N; i++ {
+		c := new(Collection)
+		c.Populate(total, count)
+
+		if (c.Digits[0].Count != 0) {
+			b.Fatal("Count of 0 should be 0\n")
+		}
+	}
+}
