1b3295de4b7ae0d5addc719f5b85c0b5f61d5d64
Anton Lindström <lindztr@gmail.com>
2013-11-13 15:08:14 +0100

    go fmt on benford_test

--- benford_test.go
+++ benford_test.go
@@ -4,19 +4,19 @@ import "testing"
 
 func TestPopulate(t *testing.T) {
 	total := 29
-	count := []int{0,9,5,4,2,2,2,1,2,1}
+	count := []int{0, 9, 5, 4, 2, 2, 2, 1, 2, 1}
 
 	c := new(Collection)
 	c.Populate(total, count)
 
-	if (c.Digits[0].Count != 0) {
+	if c.Digits[0].Count != 0 {
 		t.Fatal("Count of 0 should be 0\n")
 	}
 }
 
 func BenchmarkPopulate(b *testing.B) {
 	total := 29
-	count := []int{0,9,5,4,2,2,2,1,2,1}
+	count := []int{0, 9, 5, 4, 2, 2, 2, 1, 2, 1}
 
 	for i := 0; i < b.N; i++ {
 		c := new(Collection)
